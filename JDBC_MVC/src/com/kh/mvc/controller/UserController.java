1ecef04eb6a164711b368b419f230f1cc3ee3b83
hanesul1028 <khs1028ryu@naver.com>
2025-03-04 17:47:36 +0900

    20250304_JDBC_MVC

---
+++ JDBC_MVC/src/com/kh/mvc/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.kh.mvc.controller;
+
+/*
+ * VIEW에서 온 요청을 처리해주는 클래스
+ * 메소드로 전달된 데이터 값을 가공처리 한 후 DAO로 전달
+ * DAO로부터 반환받은 결과를 사용자가 보게 될 View (응답화면)에 반환
+ */
+public class UserController {
+
+	public void findAll() {
+		
+	}
+}
